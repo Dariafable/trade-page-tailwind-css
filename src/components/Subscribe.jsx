c56d25edfe509972bba8282b4fc4873e9550c399
Daria Nebylitsa <daranebylitsa@gmail.com>
2022-08-14 19:47:52 +0300

    'fix some text'

--- src/components/Subscribe.jsx
+++ src/components/Subscribe.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Subscribe = () => {
   return (
     <div className='w-full px-4 py-16 bg-black text-white text-center'>
-      <h1>Join Our DeFi Communit</h1>
+      <h1>Join Our DeFi Community</h1>
       <div className='py-4'>
         <input className='p-3 rounded-3xl mr-4 text-[#6a00ff]' type='email' placeholder='Enter your email' />
         <button>Sign Up</button>
